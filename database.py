8401d84742886f08e7b16000a317e718a4bb33e5
Ben Harris <ben@boxersteavee.dev>
2025-09-24 09:52:50 +0100

    new device woot woot

---
+++ database.py
@@ -0,0 +1,10 @@
+import sqlite3 as sql
+
+# class for database
+# initialise DB (connect, which creates the file if it doesn't exist)
+# create tables if they don't already exist.
+
+class Database():
+    def __init__(self, db_path):
+
+    def create_fam_db(self):
