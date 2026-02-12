cf277a4bd09f251280cee0877a6893dea5e13907
Boxersteavee <76684157+Boxersteavee@users.noreply.github.com>
2026-02-12 23:35:58 +0000

    - Add a filter for unlinked individuals

--- sql2json.py
+++ sql2json.py
@@ -57,14 +57,63 @@ def jsonify(individuals):
         jsonified.append(entry)
     return jsonified
 
+# Sometimes, gedcom files contain people which have no connections.
+# These should not be displayed as they have no connections,
+# and cause clutter when rendered.
+# This function exists to filter them out.
+def remove_isolated_individuals(individuals):
+    # Builds a list of parents
+    # If someone ends up in this list then they have
+    # at least one child,so should not be filtered
+    parent_ids = []
+    for individual in individuals:
+        if individual['mid'] is not None:
+            if individual['mid'] not in parent_ids:
+                parent_ids.append(individual['mid'])
 
+        if individual['fid'] is not None:
+            if individual['fid'] not in parent_ids:
+                parent_ids.append(individual['fid'])
+
+    # Builds a list of all people who has a parent,
+    # has a partner or is a parent.
+    filtered = []
+    for individual in individuals:
+        has_parents = False
+        has_partners = False
+        is_parent = False
+        # Checks their mother_id or father_id field is not empty.
+        # If one of them is not, they are a child!
+        if individual['mid'] is not None or individual['fid'] is not None:
+            has_parents = True
+
+        # Checks if their list of Partner IDs is not empty.
+        # If it isn't, they have a partner connection
+        if len(individual['pids']) > 0:
+            has_partners = True
+
+        # Checks if their ID is in parent_ids,
+        # if it is, they have a child.
+        if individual['id'] in parent_ids:
+            is_parent = True
+
+        # If any of the above checks return true,
+        # they have at least one connection so are okay to display.
+        if has_parents or has_partners or is_parent:
+            filtered.append(individual)
+
+    return filtered
+
+# This function is called by the API
+# to create the json response from a tree.
 def run(tree):
     db_path = cfg['db_dir'] + "/" + tree + ".db"
     if not os.path.isfile(db_path):
         return 404
     db = Database(db_path)
     individuals = get_individuals_data(db)
-    output = jsonify(individuals)
+    jsonified = jsonify(individuals)
+    output = remove_isolated_individuals(jsonified)
     return output
 
 if __name__ == "__main__":
