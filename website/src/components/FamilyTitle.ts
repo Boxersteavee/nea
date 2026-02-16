f49b89412adee68953368670a8330e383045651f
Boxersteavee <76684157+Boxersteavee@users.noreply.github.com>
2026-02-16 16:46:52 +0000

    - Add comments

--- website/src/components/FamilyTitle.ts
+++ website/src/components/FamilyTitle.ts
@@ -1,3 +1,4 @@
+// This is a function to get the FamilyTitle from the API and return it in data.name
 export async function getFamilyTitle(baseUrl: string): Promise<string> {
     const apiUrl = new URL('/api/config/name', baseUrl);
     const response = await fetch(apiUrl);
